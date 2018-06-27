9844a9dc37cae702909ea9742d317ece3c0f3c7a
thomas <thomas.rachavong@affluences.com>
2018-06-27 14:14:48 +0200

    remove HttpCientModule,

--- src/providers/company/company.ts
+++ src/providers/company/company.ts
@@ -1,4 +1,4 @@
-import {HttpCientModule, HttpClient} from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { company } from '../../model/company'; 
 import 'rxjs/add/operator/toPromise';
