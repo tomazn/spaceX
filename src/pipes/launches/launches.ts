de103db026198f9006ad7c6f895a492ac0de6d99
thomas <thomas.rachavong@affluences.com>
2018-07-01 00:13:51 +0200

    filter launches works

--- src/pipes/launches/launches.ts
+++ src/pipes/launches/launches.ts
@@ -14,12 +14,50 @@ export class LaunchesPipe implements PipeTransform {
    */
   transform(value: any, selectedSuccessLaunch: string, selectedRocketsFilter: string, selectedLaunchDateFilter: string) {
 
-console.log(selectedSuccessLaunch);
-console.log(selectedRocketsFilter);
-console.log(selectedLaunchDateFilter);
-
     if((!selectedSuccessLaunch || selectedSuccessLaunch == "all") && (!selectedRocketsFilter || selectedRocketsFilter == "all") && (!selectedLaunchDateFilter || selectedLaunchDateFilter == "all")){
       return value;
-    } 
+    }
+
+
+    let filteredLaunches: any[] = [];
+    
+    let successValidation;
+    let rocketValidation;
+    let dateValidation;
+
+    for(let launch of value){
+
+      successValidation = false;
+      rocketValidation = false;
+      dateValidation = false;
+
+      //Validation for success launch
+      if(selectedSuccessLaunch == "all"){
+        successValidation = true;
+      }else if(launch.launch_success.toString() == selectedSuccessLaunch){
+        successValidation = true;
+      }
+
+      //Validation for rocket 
+      if(selectedRocketsFilter == "all"){
+        rocketValidation = true;
+      }else if(launch.rocket.rocket_name == selectedRocketsFilter){
+        rocketValidation = true;
+      }
+
+      //Validation for date 
+      if(selectedLaunchDateFilter == "all"){
+        dateValidation = true;
+      }else if(launch.launch_year == selectedLaunchDateFilter){
+        dateValidation = true;
+      }
+
+      if(successValidation && rocketValidation && dateValidation){
+        filteredLaunches.push(launch);
+      }
+    }
+
+    console.log(filteredLaunches);
+    return filteredLaunches;
   }
 }
